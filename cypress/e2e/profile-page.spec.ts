2dd1dc3ee6094ae5a67cc24a8db63d1a978d43a4
Pierre_Tsia <pierre.tsiakkaros@gmail.com>
2022-11-20 12:15:58 +0100

    test(e2e): add fixtures to mock api call

--- cypress/e2e/profile-page.spec.ts
+++ cypress/e2e/profile-page.spec.ts
@@ -1,10 +1,18 @@
 context("Profile Page", () => {
+  const BASE_API_URL = "https://ayfkzjkikyewseeewwve.supabase.co/rest/v1/";
+  const GET_USER_BY_ID = "users?select=*&id=eq.1";
+  const GET_USER_API_URL = BASE_API_URL + GET_USER_BY_ID;
+
   beforeEach(() => {
     cy.visit("/profile/1");
   });
 
   it("profile-page base elements", () => {
     cy.url().should("eq", "http://localhost:3333/profile/1");
+
+    cy.intercept("GET", GET_USER_API_URL, {
+      fixture: "users.json",
+    }).as("getUserById");
 
     cy.contains("Cersei Lannister").should("exist");
   });
